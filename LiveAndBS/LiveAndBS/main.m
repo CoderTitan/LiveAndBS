d110078f11471167679bf22bd49074a35905bb50
田全军 <tianquanjun@tianquanjundemacbook-pro.local>
2017-02-12 14:31:50 +0800

    初始化项目框架

---
+++ LiveAndBS/LiveAndBS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LiveAndBS
+//
+//  Created by 田全军 on 17/2/12.
+//  Copyright © 2017年 Quanjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
