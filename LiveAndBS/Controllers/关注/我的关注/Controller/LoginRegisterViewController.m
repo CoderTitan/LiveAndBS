86779e554447de01268ad329e787b987fb4e4f5c
CoderJun <quanjunt@163.com>
2017-10-10 07:47:15 +0800

    删除冗余代码，模块优化

--- LiveAndBS/Controllers/关注/我的关注/Controller/LoginRegisterViewController.m
+++ LiveAndBS/Controllers/关注/我的关注/Controller/LoginRegisterViewController.m
@@ -19,7 +19,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
 }
 
 //调节状态栏颜色为白色
@@ -55,20 +55,5 @@
 - (IBAction)tengxunLoginClick:(id)sender {
 
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
